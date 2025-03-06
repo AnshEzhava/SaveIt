ef006bb98eb11f08ecc1e75bafea5ddc3ad9680c
Ansh Ezhava <anshdpsg@gmail.com>
2025-03-07 00:07:25 +0530

    feat: add folder and nofolder components with basic structure and styles

---
+++ src/app/pages/nofolder/nofolder.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nofolder',
+  standalone: true,
+  imports: [],
+  templateUrl: './nofolder.component.html',
+  styleUrl: './nofolder.component.css'
+})
+export class NofolderComponent {
+
+}
