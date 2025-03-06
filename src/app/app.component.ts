6c3d1b0b62e3da7de978b029949147bcf567106d
Ansh Ezhava <anshdpsg@gmail.com>
2025-03-06 21:15:56 +0530

    feat: add GitHub link button and update button styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,8 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent {
+  openGithub() {
+    window.open('https://github.com/AnshEzhava/SaveIt', '_blank');
+  }
+}
