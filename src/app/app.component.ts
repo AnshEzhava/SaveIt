d475c379b828bd818e19a5eb34fac3b7ef7ee539
AnshEzhava <anshdpsg@gmail.com>
2025-03-07 11:45:24 +0530

    feat: add folder component and update app structure; remove unused components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FolderComponent } from './components/folder/folder.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FolderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  activePage: string = '';
+  activePage: string = 'folders';
   openGithub() {
     window.open('https://github.com/AnshEzhava/SaveIt', '_blank');
   }
